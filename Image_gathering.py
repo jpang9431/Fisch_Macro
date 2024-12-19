0a6771ae60b074dcf03f15f7819cc443f461d983
Bida L <bida2023@gmail.com>
2024-12-18 21:54:32 -0500

    Improved Image_gathering for production

--- Image_gathering.py
+++ Image_gathering.py
@@ -4,6 +4,7 @@ import keyboard
 import pyautogui
 import pyscreeze
 import screeninfo
+from datetime import datetime
 from screeninfo import get_monitors
 monitor = get_monitors()[0]
 print(str(monitor))
@@ -19,7 +20,15 @@ region = (left,height-1,right-left,1)
 counter = 0
 
 
-def getScreenShot(name:str,path=""):
+def getScreenShotSaved(name:str,path=""):
+    currentTime = datetime.now()
     pyautogui.screenshot(imageFilename=path+name+"_full.png")
+    elapsedTime1 = datetime.now()- currentTime
+    currentTime = datetime.now()
     pyautogui.screenshot(imageFilename=path+name+"_small.png",region=region)
+    elapsedTime2 = datetime.now() - currentTime
+    print("Time to take full screenshot: "+str(elapsedTime1))
+    print("Time to take small screenshot: "+str(elapsedTime2))
 
+def getScreenShot(name:str,path="") -> Image:
+    return pyautogui.screenshot(region=region)
