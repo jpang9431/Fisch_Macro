164f777f4ec7de8abf39cd094fd70b2c5a300517
SuperExago <69761561+jpang9431@users.noreply.github.com>
2024-12-18 21:11:16 -0500

    Limited the area of the screenshots

--- Image_gathering.py
+++ Image_gathering.py
@@ -1,10 +1,23 @@
 import keyboard
 import pyautogui
 import pyscreeze
+import screeninfo
+from screeninfo import get_monitors
+monitor = get_monitors()[0]
+print(str(monitor))
+width = monitor.width
+height = monitor.height
+
+left = int(width * .3)
+right = int(width * .7)
+height = int(height * .85)
+
+region = (left,height-1,right-left,1)
 
 counter = 0
+
 while True:
     keyboard.wait("e")
-    screenshot = pyautogui.screenshot(str(counter)+".png")
+    screenshot = pyautogui.screenshot(imageFilename=str(counter)+".png",region=region)
     counter+=1
     print("e")
